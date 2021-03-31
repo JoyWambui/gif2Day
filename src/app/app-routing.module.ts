e0ef0b840bfdf6be66fb109e26fdad6d3f8152f4
JoyWambui <JoyWambui@github.com>
2021-03-31 20:10:22 +0300

    add routes to component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {SearchComponent} from './search/search.component';
+import{TrendingComponent} from './trending/trending.component';
+const routes: Routes = [
+  {path: '', redirectTo:'/trending', pathMatch:'full'},
+  {path: 'search', component: SearchComponent},
+  {path: 'trending', component: TrendingComponent}
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+ 
