b9f380a80fe488ede1d5448382fde528e0c5fe15
JoyWambui <JoyWambui@github.com>
2021-03-31 19:00:10 +0300

    create search and trending components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { SearchComponent } from './search/search.component';
+import { TrendingComponent } from './trending/trending.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    SearchComponent,
+    TrendingComponent
   ],
   imports: [
     BrowserModule,
