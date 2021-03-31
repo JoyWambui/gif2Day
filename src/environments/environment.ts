fb27695b3f0b7c448963b2bba72c66d283be3ae9
JoyWambui <JoyWambui@github.com>
2021-03-31 20:55:50 +0300

    add apikey to environment file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: 'u0Gctjjh4bLoLJdPeK95MuW8HBmrQJYd'
 };
 
 /*
